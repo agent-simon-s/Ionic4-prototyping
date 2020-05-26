115537a78379c055f86a117e9a7535cd237402d8
sutherland simon <simon.sutherland@capgemini.com>
2020-05-26 03:49:34 -0700

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	dev: true,
+	verbose: true,
+	SendGrid: {
+	   LOGIN: {
+	      USERNAME: 'XXXXXXXXXXXXXXXXXXXXXX',
+	      PASSWORD: 'XXXXXXXXXXXXXXXXXXXXXX'
+	   }
+	},
+	Flurry: {
+		your_app_key: 'XXXXXXXXXXXXXXXXXXXXXX'
+	}
 };
 
 /*
