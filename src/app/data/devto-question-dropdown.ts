7423c833654b58925ea849c988ceb8cdfb88e0dd
sutherland simon <simon.sutherland@capgemini.com>
2020-05-26 03:57:46 -0700

    comment out data/devto-question-dropdow for ocmpile fix

--- src/app/data/devto-question-dropdown.ts
+++ src/app/data/devto-question-dropdown.ts
@@ -1,11 +1,11 @@
-import { QuestionBase } from './devto-question-base';
+// import { QuestionBase } from './devto-question-base';
 
-export class DropdownQuestion extends QuestionBase<string> {
-  controlType = 'dropdown';
-  options: { key: string, value: string }[] = [];
+// export class DropdownQuestion extends QuestionBase<string> {
+//   controlType = 'dropdown';
+//   options: { key: string, value: string }[] = [];
 
-  constructor(options: {} = {}) {
-    super(options);
-    this.options = options['options'] || [];
-  }
-}
+//   constructor(options: {} = {}) {
+//     super(options);
+//     this.options = options['options'] || [];
+//   }
+// }
